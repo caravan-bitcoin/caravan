e67e82950a5b2dbaf43f41da004d1a4ce40ee5c5
buck <buck@unchained.com>
2024-01-31 16:49:27 -0600

    setup shared configs

--- packages/eslint-config/library.js
+++ packages/eslint-config/library.js
@@ -4,14 +4,19 @@ const project = resolve(process.cwd(), "tsconfig.json");
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: ["eslint:recommended", "prettier", "eslint-config-turbo"],
-  plugins: ["only-warn"],
-  globals: {
-    React: true,
-    JSX: true,
-  },
   env: {
     node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
   },
   settings: {
     "import/resolver": {
@@ -20,15 +25,7 @@ module.exports = {
       },
     },
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-    "dist/",
-  ],
-  overrides: [
-    {
-      files: ["*.js?(x)", "*.ts?(x)"],
-    },
-  ],
+  rules: {
+    "@typescript-eslint/no-non-null-assertion": "off",
+  },
 };
