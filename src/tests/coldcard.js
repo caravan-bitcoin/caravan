d6ad83ff557505f6a1f15ec2ed794e967cf5e7d3
bucko13 <buck.perley@protonmail.com>
2024-02-15 21:55:48 +0000

    Deploying to gh-pages from @ caravan-bitcoin/caravan@c6976cbe991dcf4015ee9d0835a2b8b5204f3c1a 🚀

--- src/tests/coldcard.js
+++
@@ -1,9 +0,0 @@
-import { COLDCARD } from "unchained-wallets";
-
-import extendedPublicKeyTests from "./extendedPublicKeys";
-import { signingTests } from "./signing";
-import publicKeyTests from "./publicKeys";
-
-export default publicKeyTests(COLDCARD)
-  .concat(extendedPublicKeyTests(COLDCARD))
-  .concat(signingTests(COLDCARD));
