601ea32ffcc92468b213f1b3ebfd5a0c7471c215
buck <buck@unchained.com>
2024-02-15 15:08:54 -0600

    Updates

--- src/components/Hermit/index.js
+++
@@ -1,5 +0,0 @@
-import HermitReader from "./HermitReader";
-import HermitDisplayer from "./HermitDisplayer";
-import HermitPsbtInterface from "./HermitPsbtInterface";
-
-export { HermitDisplayer, HermitReader, HermitPsbtInterface };
--- src/components/Slices/index.js
+++
@@ -1,4 +0,0 @@
-export { default as SlicesTable } from "./SlicesTable";
-export { default as SlicesTableContainer } from "./SlicesTableContainer";
-export { default as SliceDetails } from "./SliceDetails";
-export { default as ConfirmAddress } from "./ConfirmAddress";
