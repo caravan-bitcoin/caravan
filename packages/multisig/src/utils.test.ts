eed880145815c520e69e79d4375622aed7971f59
buck <buck@unchained.com>
2024-05-07 13:59:41 -0500

    add multisig tests

---
+++ packages/multisig/src/utils.test.ts
@@ -0,0 +1,19 @@
+import { TEST_FIXTURES } from "@caravan/bitcoin";
+import { MultisigWalletConfig } from "./types";
+import { braidDetailsToWalletConfig } from "./utils";
+
+describe("braidDetailsToWalletConfig", () => {
+  TEST_FIXTURES.transactions.forEach((fixture) => {
+    it("can convert braid details to a wallet config", () => {
+      const walletConfig: MultisigWalletConfig = braidDetailsToWalletConfig(
+        fixture.braidDetails,
+      );
+      console.log(walletConfig);
+      expect(walletConfig).toHaveProperty("network");
+      expect(walletConfig).toHaveProperty("addressType");
+      expect(walletConfig).toHaveProperty("extendedPublicKeys");
+      expect(walletConfig).toHaveProperty("quorum");
+      expect(walletConfig).toHaveProperty("name");
+    });
+  });
+});
