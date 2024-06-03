4f17031f4443e6d7fc36c168583c89fe34ccdc13
buck <buck@unchained.com>
2024-06-03 11:43:45 -0500

    lint fix

--- apps/coordinator/src/components/ScriptExplorer/Transaction.jsx
+++ apps/coordinator/src/components/ScriptExplorer/Transaction.jsx
@@ -3,7 +3,6 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import {
   blockExplorerTransactionURL,
-  Network,
   addSignaturesToPSBT,
 } from "@caravan/bitcoin";
 
