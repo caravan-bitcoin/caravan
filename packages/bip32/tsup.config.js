11ef1db9962bb48b6124b88bcff3210c9da59f37
buck <buck@unchained.com>
2024-09-05 22:23:03 -0500

    custom util for getting random ints in node or browser

--- packages/bip32/tsup.config.js
+++ packages/bip32/tsup.config.js
@@ -2,5 +2,5 @@ import { polyfillNode } from "esbuild-plugin-polyfill-node";
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  esbuildPlugins: [polyfillNode({ polyfills: { crypto: true } })],
+  esbuildPlugins: [polyfillNode({ polyfills: { crypto: false } })],
 });
