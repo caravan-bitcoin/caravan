070c97d6800e9b6a957495cf8fb5d20337784bbb
Dhairya Majmudar <124715224+DhairyaMajmudar@users.noreply.github.com>
2024-09-10 08:19:26 +0530

    [Test]: Jest to Vitest migration `caravan-client` package (#119)
    * jest to vitest migration of caravan-client
    
    * updating pacakge-lock.json
    
    * moving test files into  folder
    
    * making watch flag false
    
    * moving vitest config to root
    
    * moving vitest config to root

--- packages/caravan-clients/src/__tests__/wallet.test.ts
+++ packages/caravan-clients/src/__tests__/wallet.test.ts
@@ -4,9 +4,11 @@ import {
   bitcoindImportDescriptors,
   bitcoindGetAddressStatus,
   bitcoindListUnspent,
-} from "./wallet";
+} from "../wallet";
 
-import * as bitcoind from "./bitcoind";
+import { beforeEach, afterEach, vi, describe, it, expect } from "vitest";
+
+import * as bitcoind from "../bitcoind";
 import BigNumber from "bignumber.js";
 
 describe("Wallet Functions", () => {
@@ -20,12 +22,12 @@ describe("Wallet Functions", () => {
   const expectedWalletPath = `${baseUrl}/wallet/${walletName}`;
 
   beforeEach(() => {
-    mockCallBitcoind = jest.fn();
-    jest.spyOn(bitcoind, "callBitcoind").mockImplementation(mockCallBitcoind);
+    mockCallBitcoind = vi.fn();
+    vi.spyOn(bitcoind, "callBitcoind").mockImplementation(mockCallBitcoind);
   });
 
   afterEach(() => {
-    jest.clearAllMocks();
+    vi.clearAllMocks();
   });
 
   describe("callBitcoindWallet", () => {
@@ -151,7 +153,7 @@ describe("Wallet Functions", () => {
     });
 
     it("should throw an error if no result", async () => {
-      const consoleErrorMock = jest.spyOn(console, "error");
+      const consoleErrorMock = vi.spyOn(console, "error");
       consoleErrorMock.mockImplementation(() => {});
       mockCallBitcoind.mockResolvedValue({ result: undefined });
       const address = "address";
