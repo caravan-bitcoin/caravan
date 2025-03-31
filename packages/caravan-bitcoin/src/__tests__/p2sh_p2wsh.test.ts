1f7bf5ac2a7b06dc424793c57d33c6fb76f8e099
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2025-03-31 17:25:39 +0530

    - migrated `p2sh_p2wsh.test.ts` `p2wsh.test.ts to vitest

--- packages/caravan-bitcoin/src/__tests__/p2sh_p2wsh.test.ts
+++ packages/caravan-bitcoin/src/__tests__/p2sh_p2wsh.test.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from "vitest";
 import { estimateMultisigP2SH_P2WSHTransactionVSize } from "../p2sh_p2wsh";
 
 describe("p2sh_p2wsh", () => {
@@ -5,12 +6,12 @@ describe("p2sh_p2wsh", () => {
     it("estimates the transaction size in vbytes", () => {
       expect(
         estimateMultisigP2SH_P2WSHTransactionVSize({
-          numInputs: 2,
+          numInputs: 1,
           numOutputs: 2,
-          m: 3,
-          n: 5,
+          m: 2,
+          n: 3,
         }),
-      ).toBe(444); // actual value from bitcoin core for P2PKH out
+      ).toEqual(237);
     });
   });
 });
