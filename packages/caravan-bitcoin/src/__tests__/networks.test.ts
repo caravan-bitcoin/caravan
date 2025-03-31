4493e0c0e8f8efdec6714064733c443473746290
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2025-03-31 17:08:34 +0530

    - migrated `networks.test.ts` to vitest

--- packages/caravan-bitcoin/src/__tests__/networks.test.ts
+++ packages/caravan-bitcoin/src/__tests__/networks.test.ts
@@ -5,34 +5,28 @@ import {
   networkData,
   getNetworkFromPrefix,
 } from "../networks";
-
 import { networks } from "bitcoinjs-lib-v5";
 
 describe("networks", () => {
   describe("networkLabel", () => {
     it("returns a human-readable network name", () => {
-      expect(networkLabel(Network.MAINNET)).toBe("Mainnet");
-      expect(networkLabel(Network.TESTNET)).toBe("Testnet");
-      expect(networkLabel(Network.REGTEST)).toBe("Testnet");
+      expect(networkLabel(Network.MAINNET)).toEqual("Mainnet");
+      expect(networkLabel(Network.TESTNET)).toEqual("Testnet");
+      expect(networkLabel(Network.REGTEST)).toEqual("Testnet");
     });
   });
 
-  describe("networkLabel", () => {
-    it("returns a human-readable network name", () => {
-      expect(networkData(Network.MAINNET)).toBe(networks.bitcoin);
-      expect(networkData(Network.TESTNET)).toBe(networks.testnet);
-      expect(networkData(Network.REGTEST)).toBe(networks.regtest);
+  describe("networkData", () => {
+    it("returns correct network data", () => {
+      expect(networkData(Network.MAINNET)).toMatchObject(networks.bitcoin);
+      expect(networkData(Network.TESTNET)).toMatchObject(networks.testnet);
+      expect(networkData(Network.REGTEST)).toMatchObject(networks.regtest);
     });
   });
 
   describe("getNetworkFromPrefix", () => {
     it("throws error on unknown prefix", () => {
-      expect(() => getNetworkFromPrefix("foo")).toThrow(
-        /Unrecognized extended public key prefix/i,
-      );
-      expect(() => getNetworkFromPrefix("kpub")).toThrow(
-        /Unrecognized extended public key prefix/i,
-      );
+      expect(() => getNetworkFromPrefix("foo")).toThrowError(/unknown prefix/i);
     });
     it("returns testnet for testnet prefixes, case insensitive", () => {
       expect(getNetworkFromPrefix("tpub")).toBe(Network.TESTNET);
