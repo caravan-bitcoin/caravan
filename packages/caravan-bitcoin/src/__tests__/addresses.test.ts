6faa479de43c838f3147b7537b6480f01dc22ea3
Shashank RM <ugs22047_ece.shashank@cbit.org.in>
2025-03-31 17:02:22 +0530

    - migrated `addresses.test.ts` to vitest

--- packages/caravan-bitcoin/src/__tests__/addresses.test.ts
+++ packages/caravan-bitcoin/src/__tests__/addresses.test.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from "vitest";
 import { validateAddress, getAddressType } from "../addresses";
 import * as multisig from "../multisig";
 import { Network } from "../networks";
