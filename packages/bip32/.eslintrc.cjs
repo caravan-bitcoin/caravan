548c8cbbb4420e63f1d6cfc11522a767ea1ed793
buck <buck@unchained.com>
2025-04-07 23:12:03 -0500

    fix: better shared eslint rules

--- packages/bip32/.eslintrc.cjs
+++ packages/bip32/.eslintrc.cjs
@@ -3,27 +3,5 @@ module.exports = {
   plugins: ["import"],
   rules: {
     "@typescript-eslint/no-duplicate-enum-values": "warn",
-    "import/order": [
-      "error",
-      {
-        groups: [
-          "builtin",
-          "external",
-          "internal",
-          "parent",
-          "sibling",
-          "index",
-        ],
-        "newlines-between": "always",
-        alphabetize: { order: "asc", caseInsensitive: true },
-      },
-    ],
-    "sort-imports": [
-      "error",
-      {
-        allowSeparatedGroups: true,
-        ignoreDeclarationSort: true,
-      },
-    ],
   },
 };
