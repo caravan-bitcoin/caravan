f97b8ce9f154d649f277df51ccd30b2068c2cd71
abhishek <abhishek4code@gmail.com>
2025-04-17 16:52:06 +0530

    chore: more configuration added to vitest config file

--- packages/bip32/vitest.config.ts
+++ packages/bip32/vitest.config.ts
@@ -2,7 +2,14 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    include: ['src/**/*.test.ts'],
+    exclude: ['node_modules/**', 'dist/**'],
     globals: true,
     environment: "node",
+    testTimeout: 10000,
+    typecheck: {
+        tsconfig: './tsconfig.json',
+        enabled: false
+    }
   },
 });
