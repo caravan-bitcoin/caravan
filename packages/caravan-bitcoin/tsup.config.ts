95a940e1a8636e70c5be8330e59f29a5dcb1fe91
buck <buck@unchained.com>
2025-04-24 17:36:26 -0500

    fix: some missing buffer polyfills (#267)
    * fix: some missing buffer polyfills
    
    * revert unnecessary tsup configs
    
    * fix(coordinator): update vite versions
    
    * fix build

--- packages/caravan-bitcoin/tsup.config.ts
+++ packages/caravan-bitcoin/tsup.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from "tsup";
-import { nodeModulesPolyfillPlugin } from "esbuild-plugins-node-modules-polyfill";
 import { provideNavigator, provideSelf } from "@caravan/build-plugins";
+import { nodeModulesPolyfillPlugin } from "esbuild-plugins-node-modules-polyfill";
+import { defineConfig } from "tsup";
 
 export default defineConfig({
   esbuildPlugins: [
