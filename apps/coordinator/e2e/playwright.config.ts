b43fc2ea15287ac0fa346160f74d1513053a966a
Abhishek Raj <abhishek4code@gmail.com>
2025-06-07 17:12:51 +0530

    fix: close preview server after playwright tests

--- apps/coordinator/e2e/playwright.config.ts
+++ apps/coordinator/e2e/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:4173',
+    baseURL: 'http://localhost:5173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -74,8 +74,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'cd .. && npm run preview',
-    url: 'http://localhost:4173',
+    command: 'cd .. && npm run dev',
+    url: 'http://localhost:5173',
     reuseExistingServer: !process.env.CI,
     timeout: 120 * 1000,
   },
---
+++ apps/coordinator/e2e/utils/globalSetup.ts
@@ -0,0 +1,6 @@
+//steps
+
+//1. import and set the correct arg
+//2. set it in the playwright-config as well
+//3. then lets say execute a simple cmd also, just for checking if thats works
+//4. then can add asimple 
