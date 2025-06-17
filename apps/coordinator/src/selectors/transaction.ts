50b4de96270b67e9ba1e5a092e8929bcc16d9882
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2025-06-17 23:50:00 +0530

    refactor: move `processInputsFromPSBT`out of transactionAction and make it a selector as it only  transforms/filters data

---
+++ apps/coordinator/src/selectors/transaction.ts
@@ -0,0 +1,39 @@
+import { reverseBuffer } from "bitcoinjs-lib/src/bufferutils";
+import { Psbt } from "bitcoinjs-lib-v6";
+import { createSelector } from "reselect";
+import { getSpendableSlices } from "./wallet";
+import { UTXO, Input, Slice } from "utils/psbtUtils";
+
+/**
+ * Processes inputs from PSBT and matches them with wallet UTXOs.
+ */
+export const selectInputsFromPSBT = (psbt: Psbt) =>
+  createSelector([getSpendableSlices], (slices: any) => {
+    const createInputIdentifier = (txid: string, index: number) =>
+      `${txid}:${index}`;
+
+    const inputIdentifiers = new Set(
+      psbt.txInputs.map((input) => {
+        const txid = reverseBuffer(input.hash).toString("hex");
+        return createInputIdentifier(txid, input.index);
+      }),
+    );
+
+    const inputs: Input[] = [];
+    slices.forEach((slice: Slice & { utxos: UTXO }) => {
+      Object.entries(slice.utxos).forEach(([, utxo]) => {
+        const inputIdentifier = createInputIdentifier(utxo.txid, utxo.index);
+        if (inputIdentifiers.has(inputIdentifier)) {
+          const input = {
+            ...utxo,
+            multisig: slice.multisig,
+            bip32Path: slice.bip32Path,
+            change: slice.change,
+          };
+          inputs.push(input);
+        }
+      });
+    });
+
+    return inputs;
+  });
