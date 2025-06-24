c1a741347b91cdba13bc70143c25f9c274e12f9e
Abhishek Raj <abhishek4code@gmail.com>
2025-06-24 22:05:00 +0530

    chore: containerized caravan build

--- apps/coordinator/e2e/playwright.config.ts
+++ apps/coordinator/e2e/playwright.config.ts
@@ -76,11 +76,4 @@ export default defineConfig({
   globalSetup: "./utils/globalSetup.ts",
   globalTeardown: "utils/globalTeardown.ts",
 
-  /* Run your local dev server before starting the tests */
-  webServer: {
-    command: 'cd .. && npm run dev',
-    url: 'http://localhost:5173',
-    reuseExistingServer: !process.env.CI,
-    timeout: 120 * 1000,
-  },
 });
