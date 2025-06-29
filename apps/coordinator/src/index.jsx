96205aa5fead21d193f9951c01d9a24ac2354d73
buck <buck@unchained.com>
2025-06-29 15:48:02 -0500

    add tanstack query support to the coordinator

--- apps/coordinator/src/index.jsx
+++ apps/coordinator/src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom";
 import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import ReduxPromise from "redux-promise";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import thunk from "redux-thunk";
 
@@ -16,9 +18,33 @@ const store = createStore(
   composeEnhancers(applyMiddleware(ReduxPromise, thunk)),
 );
 
+// Create a query client with optimized defaults for in-memory caching
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      // Cache data for 5 minutes by default
+      staleTime: 5 * 60 * 1000,
+      // Keep data in cache for 10 minutes
+      cacheTime: 10 * 60 * 1000,
+      // Retry failed requests 3 times
+      retry: 3,
+      // Don't refetch on window focus by default
+      refetchOnWindowFocus: false,
+      // Don't refetch on reconnect by default
+      refetchOnReconnect: false,
+    },
+  },
+});
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      {/* Only show devtools in development */}
+      {process.env.NODE_ENV === "development" && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
+    </QueryClientProvider>
   </Provider>,
   document.getElementById("app"),
 );
