8d22ab5178b1b731858b2fd08683e7d3a1b0aa44
buck <buck@unchained.com>
2025-07-01 06:47:03 -0500

    Fix waste calculations (#318)

--- packages/caravan-bitcoin/src/p2sh.test.ts
+++ packages/caravan-bitcoin/src/p2sh.test.ts
@@ -10,7 +10,27 @@ describe("p2sh", () => {
           m: 2,
           n: 3,
         }),
-      ).toBe(387);
+      ).toBe(371); // from bitcoin optech calculator
+    });
+    it("estimates the transaction size in vbytes for 253 inputs", () => {
+      expect(
+        estimateMultisigP2SHTransactionVSize({
+          numInputs: 254,
+          numOutputs: 2,
+          m: 2,
+          n: 3,
+        }),
+      ).toBe(75514);
+    });
+    it("estimates the transaction size in vbytes for 253 outputs", () => {
+      expect(
+        estimateMultisigP2SHTransactionVSize({
+          numInputs: 1,
+          numOutputs: 253,
+          m: 2,
+          n: 3,
+        }),
+      ).toBe(8405);
     });
   });
 });
