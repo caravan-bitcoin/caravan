9dac214910380b80f1ce29c1ee549cff75e26e68
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2025-07-02 19:13:54 +0530

    fix:ci

--- apps/coordinator/src/selectors/transaction.ts
+++ apps/coordinator/src/selectors/transaction.ts
@@ -10,11 +10,11 @@ import {
   SignatureInfo,
   SignatureSet,
   SignerIdentity,
-} from "utils/psbtUtils";
+} from "../utils/psbtUtils";
 import {
   createInputIdentifier,
   convertTxidToLittleEndian,
-} from "utils/transactionCalculations";
+} from "../utils/transactionCalculations";
 
 /**
  * Maps extracted signature sets to Caravan's signature importer format.
