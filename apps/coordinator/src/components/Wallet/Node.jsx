f93ecb4dc60f8638c37fcc26ffe7038662fe3ed3
Armaan Shukla <ice-009@Mac.lan>
2025-07-03 13:13:45 +0530

    SWA Slider | Node.jsx  comments resolved

--- apps/coordinator/src/components/Wallet/Node.jsx
+++ apps/coordinator/src/components/Wallet/Node.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import PropTypes from "prop-types";
-import { connect, useSelector } from "react-redux";
+import { connect } from "react-redux";
 import { satoshisToBitcoins } from "@caravan/bitcoin";
 
 // Components
@@ -15,82 +15,121 @@ import {
 } from "../../actions/transactionActions";
 import { WALLET_MODES } from "../../actions/walletActions";
 
-const Node = ({
-  addNode,
-  addressKnown,
-  balanceSats,
-  bip32Path,
-  braidNode,
-  change,
-  fetchedUTXOs,
-  inputs,
-  multisig,
-  present,
-  setInputs,
-  spend,
-  updateAutoSpend,
-  updateNode,
-  utxos,
-  walletMode,
-}) => {
-  const [indeterminate, setIndeterminate] = useState(false);
-  const [checked, setChecked] = useState(false);
+class Node extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      indeterminate: false,
+      checked: false,
+    };
+  }
 
-  const feeRate = useSelector((state) => state.spend.transaction.feeRate);
-
-  useEffect(() => {
-    generate();
-  }, []);
+  componentDidMount = () => {
+    this.generate();
+  };
 
   // Passing this fn down to the UTXOSet so we can get updates here, from there.
-  const setSpendCheckbox = (value) => {
+  setSpendCheckbox = (value) => {
+    const { spend } = this.props;
     if (value === "indeterminate") {
-      setIndeterminate(true);
-      setChecked(false);
-      markSpending(true);
+      this.setState({ indeterminate: true, checked: false });
+      this.markSpending(true);
     } else if (value === spend) {
       // handles select/de-select all as well as have selected some and click select all
-      setIndeterminate(false);
-      setChecked(value);
-      markSpending(value);
+      this.setState({ indeterminate: false, checked: value });
+      this.markSpending(value);
     } else {
       // handles the case of de-selecting one-by-one until there's nothing left selected
       // or there's only one utxo and we're selecting to spend it or not instead of at
       // the top level select-all or deselect-all
-      setIndeterminate(false);
-      setChecked(value);
-      markSpending(value);
+      this.setState({ indeterminate: false, checked: value });
+      this.markSpending(value);
     }
   };
 
-  const markSpending = (value) => {
+  markSpending = (value) => {
+    const { change, bip32Path, updateNode } = this.props;
     updateNode(change, { spend: value, bip32Path });
   };
 
-  const maxUtxoDate = () => {
+  render = () => {
+    const {
+      bip32Path,
+      fetchedUTXOs,
+      balanceSats,
+      multisig,
+      utxos,
+      walletMode,
+      addressKnown,
+    } = this.props;
+    const { indeterminate, checked } = this.state;
+    const spending = walletMode === WALLET_MODES.SPEND;
+    return (
+      <TableRow key={bip32Path}>
+        {spending && (
+          <TableCell>
+            <Checkbox
+              id={bip32Path}
+              name="spend"
+              onChange={this.handleSpend}
+              checked={checked}
+              disabled={!fetchedUTXOs || balanceSats.isEqualTo(0)}
+              indeterminate={indeterminate}
+            />
+          </TableCell>
+        )}
+        <TableCell>
+          <code>{bip32Path.replace("m", "*")}</code>
+        </TableCell>
+        <TableCell>{utxos.length}</TableCell>
+        <TableCell>
+          {fetchedUTXOs && addressKnown ? satoshisToBitcoins(balanceSats) : ""}
+        </TableCell>
+        <TableCell>{this.maxUtxoDate()}</TableCell>
+
+        <TableCell>{multisig ? this.renderAddress() : "..."}</TableCell>
+      </TableRow>
+    );
+  };
+
+  maxUtxoDate = () => {
+    const { utxos } = this.props;
     if (!utxos.length) return "";
     const maxtime = Math.max(...utxos.map((utxo) => utxo.time));
     if (Number.isNaN(maxtime)) return "Pending";
     return new Date(1000 * maxtime).toLocaleDateString();
   };
 
-  const renderAddress = () => {
+  renderAddress = () => {
+    const { braidNode } = this.props;
     return (
       <AddressExpander
         node={braidNode}
-        setSpendCheckbox={setSpendCheckbox}
-        feeRate={feeRate}
+        setSpendCheckbox={this.setSpendCheckbox}
       />
     );
   };
 
-  const generate = () => {
+  generate = () => {
+    const { present, change, bip32Path, addNode } = this.props;
     if (!present) {
       addNode(change, bip32Path);
     }
   };
 
-  const handleSpend = (e) => {
+  handleSpend = (e) => {
+    const {
+      change,
+      bip32Path,
+      updateNode,
+      inputs,
+      utxos,
+      multisig,
+      setInputs,
+      updateAutoSpend,
+      setFeeRate,
+      feeRate,
+    } = this.props;
     let newInputs;
 
     if (e.target.getAttribute("data-indeterminate")) {
@@ -105,8 +144,7 @@ const Node = ({
       newInputs = newInputs.concat(
         utxos.map((utxo) => ({ ...utxo, multisig, bip32Path })),
       );
-      setIndeterminate(false);
-      setChecked(true);
+      this.setState({ indeterminate: false, checked: true });
     } else if (e.target.checked) {
       newInputs = inputs.concat(
         utxos.map((utxo) => ({ ...utxo, multisig, bip32Path })),
@@ -122,36 +160,9 @@ const Node = ({
     setInputs(newInputs);
     updateNode(change, { spend: e.target.checked, bip32Path });
     updateAutoSpend(false);
+    setFeeRate(feeRate);
   };
-
-  const spending = walletMode === WALLET_MODES.SPEND;
-
-  return (
-    <TableRow key={bip32Path}>
-      {spending && (
-        <TableCell>
-          <Checkbox
-            id={bip32Path}
-            name="spend"
-            onChange={handleSpend}
-            checked={checked}
-            disabled={!fetchedUTXOs || balanceSats.isEqualTo(0)}
-            indeterminate={indeterminate}
-          />
-        </TableCell>
-      )}
-      <TableCell>
-        <code>{bip32Path.replace("m", "*")}</code>
-      </TableCell>
-      <TableCell>{utxos.length}</TableCell>
-      <TableCell>
-        {fetchedUTXOs && addressKnown ? satoshisToBitcoins(balanceSats) : ""}
-      </TableCell>
-      <TableCell>{maxUtxoDate()}</TableCell>
-      <TableCell>{multisig ? renderAddress() : "..."}</TableCell>
-    </TableRow>
-  );
-};
+}
 
 Node.propTypes = {
   addNode: PropTypes.func.isRequired,
@@ -162,6 +173,7 @@ Node.propTypes = {
   bip32Path: PropTypes.string.isRequired,
   braidNode: PropTypes.shape({}).isRequired,
   change: PropTypes.bool.isRequired,
+  feeRate: PropTypes.string.isRequired,
   fetchedUTXOs: PropTypes.bool.isRequired,
   inputs: PropTypes.arrayOf(
     PropTypes.shape({
@@ -195,7 +207,7 @@ function mapStateToProps(state, ownProps) {
     ...state.spend.transaction,
     walletMode: state.wallet.common.walletMode,
     braidNode: braid.nodes[ownProps.bip32Path],
-    // feeRate is now accessed via useSelector, not props
+    feeRate: state.spend.transaction.feeRate,
   };
 }
 
