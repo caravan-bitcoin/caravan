3d5d600f46df64b5620780291b64c4a79bff2fd1
buck <buck@unchained.com>
2025-07-06 18:11:30 -0500

    fix slice type

--- apps/coordinator/src/selectors/wallet.ts
+++ apps/coordinator/src/selectors/wallet.ts
@@ -26,7 +26,7 @@ export interface Slice {
       output: Buffer;
       pubkeys: Buffer[];
     };
-    bip32Derivations: {
+    bip32Derivation: {
       pubkey: Buffer;
       masterFingerprint: Buffer;
       path: string;
