3ba0f8844918c0a1822e7b91c041c7cd94e7e297
Abhishek Raj <abhishek4code@gmail.com>
2025-07-08 21:19:59 +0530

    chore: migrated test functions to testhelpers dir

---
+++ apps/coordinator/e2e/testhelpers/clientHelpers.ts
@@ -0,0 +1,24 @@
+import { Page } from "@playwright/test";
+import { clientConfig } from "../utils/bitcoinClient";
+import { ClientSetupOptions } from "../utils/types";
+
+
+export default async function setupPrivateClient(page: Page, options: ClientSetupOptions ){
+    const {
+      url = 'http://localhost:8080',
+      username = clientConfig.username,
+      password = clientConfig.password
+    } = options;
+  
+    await page.goto("/#/wallet");
+    await page.click('input[name="clientType"][value="private"]');
+  
+    const urlField = page.locator('input[value="http://localhost:8332"]');
+    await urlField.fill(url);
+    
+    await page.locator('#bitcoind-username').fill(username);
+    await page.locator('#bitcoind-password').fill(password);
+    
+    await page.click('button:has-text("Test Connection")');
+  
+  }
--- apps/coordinator/e2e/tests/caravan_test.spec.ts
+++ apps/coordinator/e2e/tests/caravan_test.spec.ts
@@ -1,28 +1,8 @@
 import { test, expect, Page } from "@playwright/test";
 import bitcoinClient from "../utils/bitcoinClient";
 import { clientConfig } from "../utils/bitcoinClient";
-import { ClientSetupOptions } from "../utils/types";
+import setupPrivateClient from "../testhelpers/clientHelpers"
 
-
-async function setupPrivateClient(page: Page, options: ClientSetupOptions ){
-  const {
-    url = 'http://localhost:8080',
-    username = clientConfig.username,
-    password = clientConfig.password
-  } = options;
-
-  await page.goto("/#/wallet");
-  await page.click('input[name="clientType"][value="private"]');
-
-  const urlField = page.locator('input[value="http://localhost:8332"]');
-  await urlField.fill(url);
-  
-  await page.locator('#bitcoind-username').fill(username);
-  await page.locator('#bitcoind-password').fill(password);
-  
-  await page.click('button:has-text("Test Connection")');
-
-}
 
 test.describe("Caravan Wallet Creation", () => {
   let testWallets: any[] = [];
