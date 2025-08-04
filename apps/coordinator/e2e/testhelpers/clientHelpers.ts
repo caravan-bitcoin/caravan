3c620372753d1fcbada4e83ac67f3f2fbb4fbf66
Abhishek Raj <abhishek4code@gmail.com>
2025-08-04 18:31:15 +0530

    chore: tests expected msg changed accor to coordinator

--- apps/coordinator/e2e/testhelpers/clientHelpers.ts
+++ apps/coordinator/e2e/testhelpers/clientHelpers.ts
@@ -14,14 +14,15 @@ export default async function setupPrivateClient(page: Page, options: ClientSetu
   } = options;
 
     await page.goto("/#/wallet");
-    await page.click('input[name="clientType"][value="private"]');
+    await page.locator("input[name='network'][value='regtest']").setChecked(true);
   
-    const urlField = page.locator('input[value="http://localhost:8332"]');
+    const urlField = page.locator('input[value="http://localhost:18443"]');
     await urlField.fill(url);
     
     await page.locator('#bitcoind-username').fill(username);
     await page.locator('#bitcoind-password').fill(password);
     await page.locator("#wallet-name").fill(watcherWallet)
+
     
     await page.click('button:has-text("Test Connection")');
   
