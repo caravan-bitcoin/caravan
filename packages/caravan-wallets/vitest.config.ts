115e8325275b9b4be7fd432d0955f6a0e48813e1
buck <buck@unchained.com>
2025-11-12 09:49:43 -0600

    Fix jade tests (#408)
    * fix(wallets): bad mocking and no prettier run
    
    * fix(wallets): final tests

--- packages/caravan-wallets/vitest.config.ts
+++ packages/caravan-wallets/vitest.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     globals: true,
-    environment: 'node',
-    setupFiles: ['./vitest.setup.ts'],
-    exclude: ['node_modules/**', "lib/**"],
+    environment: "node",
+    setupFiles: ["./vitest.setup.ts"],
+    exclude: ["node_modules/**", "lib/**"],
     coverage: {
       provider: "istanbul",
       reporter: ["text"],
     },
-  }
-})
+  },
+});
