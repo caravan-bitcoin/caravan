b7d97135c48a7d47cce421e584b38a1866e6d425
Legend101Zz <96632943+Legend101Zz@users.noreply.github.com>
2025-12-18 01:19:07 +0530

    feat: export PSBT combiner

--- packages/caravan-psbt/src/index.ts
+++ packages/caravan-psbt/src/index.ts
@@ -1,3 +1,11 @@
-export { PsbtV2, getPsbtVersionNumber } from './psbtv2';
+export { PsbtV2, getPsbtVersionNumber } from "./psbtv2";
 export * from "./psbtv0";
 export * from "./constants";
+export {
+  combinePsbts,
+  validateSignedPsbt,
+  detectPsbtVersion,
+  convertPsbtToVersion,
+  type CombineResult,
+  type PsbtValidationResult,
+} from "./psbtCombiner";
