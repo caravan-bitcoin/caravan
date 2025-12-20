2239b420a2d0689e45e44a6d236b539f666f0df5
Sukuna0007Abhi <appsonly310@gmail.com>
2025-12-20 16:14:15 +0000

    chore: emit bip32 per-format dts
    Signed-off-by: Sukuna0007Abhi <appsonly310@gmail.com>

--- packages/bip32/tsup.config.js
+++ packages/bip32/tsup.config.js
@@ -4,10 +4,14 @@ import { defineConfig } from "tsup";
 export default defineConfig({
   entry: ["src/index.ts"],
   format: ["esm", "cjs"],
-  dts: true,
+  dts: false,
+  experimentalDts: true,
   clean: true,
   outExtension({ format }) {
-    return { js: format === "esm" ? ".js" : ".cjs" };
+    return {
+      js: format === "esm" ? ".js" : ".cjs",
+      dts: format === "esm" ? ".d.mts" : ".d.cts",
+    };
   },
   esbuildPlugins: [polyfillNode({ polyfills: { crypto: false } })],
 });
